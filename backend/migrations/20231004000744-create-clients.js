80702321f904027769f8318bdbeaff463aaf6fb3
Bruno Barboza <brunobarbozasilva@gmail.com>
2023-10-03 21:21:52 -0300

    criação da base de dados e da tabela usuários

---
+++ backend/migrations/20231004000744-create-clients.js
@@ -0,0 +1,38 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("usuarios", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      nome: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      senha: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        field: "created_at",
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        field: "updated_at",
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("usuarios");
+  },
+};
